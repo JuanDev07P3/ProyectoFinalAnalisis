8b88984cc3476eefadd3cf63743b36098966a847
AndersonOsbeli <andersonosbelitt555@gmail.com>
2025-05-20 23:37:01 -0600

    sistema ventas en java

--- sistemaVentas/backend(APIs)/obj/Debug/net9.0/BackendVentas.AssemblyInfo.cs
+++ sistemaVentas/backend(APIs)/obj/Debug/net9.0/BackendVentas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendVentas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b16acbd95e5af92f7e75df7ba1470442d3737d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e332e329f8cea7c231db3cb9de36c84d6608657a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendVentas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendVentas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ sistemaVentas/frontend/SISTEMA_VENTAS/src/main/java/com/mycompany/sistema_ventas/SISTEMA_VENTAS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_ventas;
+
+import Formularios.Menu;
+
+/**
+ *
+ * @author osbel
+ */
+public class SISTEMA_VENTAS {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
