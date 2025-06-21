bb3ea5359900ff5a59cbb2f86212be19e60f7bbc
AndersonLee <andersonosbelitt555@gmail.com>
2025-06-21 11:37:12 -0600

    Delete backend(APIs) directory

--- backend(APIs)/Models/Categorias.cs
+++
@@ -1,9 +0,0 @@
-namespace BackendVentas.Models
-{
-    public class Categoria
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string? Descripcion { get; set; }
-    }
-}
--- backend(APIs)/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-namespace BackendVentas.Models
-{
-    public class Login
-    {
-        public required string Nombre_Usuario { get; set; }
-        public required string Contrasena { get; set; }
-    }
-}
